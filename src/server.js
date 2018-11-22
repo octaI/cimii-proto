3037237528df91a117024dd4abc43e72f6aebcc8
tbert <tomas.bert@almundo.com>
2018-11-22 20:06:59 -0300

    production

---
+++ src/server.js
@@ -0,0 +1,14 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/cimii-proto'));
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/cimii-proto/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
